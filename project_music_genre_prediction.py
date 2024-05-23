bc0e1d47bf0c0aa4b235f8981ac42120973b670a
kys <kys@iksi.edu>
2024-05-23 11:44:35 +0300

    ref py file for streamlit

--- project_music_genre_prediction.py
+++ project_music_genre_prediction.py
@@ -55,6 +55,7 @@ from sklearn.impute import SimpleImputer
 import re
 from langdetect import detect, LangDetectException
 import os
+import streamlit as st
 
 TRAIN = "https://www.dropbox.com/scl/fi/5zy935lqpaqr9lat76ung/music_genre_train.csv?rlkey=ccovu9ml8pfi9whk1ba26zdda&dl=1"
 TEST = "https://www.dropbox.com/scl/fi/o6mvsowpp9r3k2lejuegt/music_genre_test.csv?rlkey=ac14ydue0rzlh880jwj3ebum4&dl=1"
@@ -88,7 +89,6 @@ train
 `valence` - привлекательность произведения для пользователей сервиса  
 `music_genre` - музыкальный жанр
 
-## Предварительная обработка данных
 """
 
 # pip install notebook
@@ -153,7 +153,7 @@ X_test = test.drop(columns=['instance_id'])
 plt.figure(figsize=(12, 6))
 sns.countplot(y=train['music_genre'], order=train['music_genre'].value_counts().index)
 plt.title('Распределение музыкальных жанров')
-plt.show()
+st.pyplot(plt.show())
 
 numerical_features = ['acousticness', 'track_name', 'danceability',
                       'energy', 'duration_ms', 'instrumentalness',
